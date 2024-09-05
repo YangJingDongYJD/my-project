1fcf6dbb94aa437cd06c72de5eafdba216cbb602
YJD <719098166@qq.com>
2024-09-05 10:22:17 +0800

    订单支付完成<杨景栋>

--- src/pagesOrder/detail/detail.vue
+++ src/pagesOrder/detail/detail.vue
@@ -3,6 +3,7 @@
 	import { useGuessList } from '@/composables'
 	import { ref } from 'vue'
 	import { getMemberOrderAPI } from '@/services/order';
+	import {getPayWxPayMiniPayAPI,getPayMockAPI} from '@/services/pay';
 	import type { OrderResult } from '@/types/order';
 	import { OrderState, orderStateList } from '@/services/constants'
 
@@ -81,6 +82,21 @@
 		//修改订单状态为已取消
 		order.value!.orderState = OrderState.YiQuXiao;
 	}
+	//订单支付
+	const onOrderPay = async() => {
+		if(import.meta.env.DEV){
+			//开发坏境支付
+			await getPayMockAPI({orderId:query.id});
+		}else{
+			//正式支付
+			const res = await getPayWxPayMiniPayAPI({orderId:query.id});
+			wx.requestPayment(res.result);
+			//关闭当前页面，再跳转支付页面
+		}
+		uni.redirectTo({
+			url:`/pagesOrder/payment/payment?id=${query.id}`
+		})
+	}
 </script>
 
 <template>
@@ -113,7 +129,7 @@
 							 @timeup="onTimeup"
 						/>
 					</view>
-					<view class="button">去支付</view>
+					<view class="button" @tap="onOrderPay">去支付</view>
 				</template>
 				<!-- 其他订单状态:展示再次购买按钮 -->
 				<template v-else>
@@ -140,28 +156,35 @@
 				</view>
 				<!-- 用户收货地址 -->
 				<view class="locate">
-					<view class="user"> 张三 13333333333 </view>
-					<view class="address"> 广东省 广州市 天河区 黑马程序员 </view>
+					<view class="user"> {{order.receiverContact}} {{order.receiverContact}} </view>
+					<view class="address"> {{order.receiverAddress}}</view>
 				</view>
 			</view>
 
 			<!-- 商品信息 -->
 			<view class="goods">
 				<view class="item">
-					<navigator class="navigator" v-for="item in 2" :key="item" :url="`/pages/goods/goods?id=${item}`"
-						hover-class="none">
+					<navigator 
+					  class="navigator" 
+					  v-for="item in order?.skus" 
+					  :key="item.id" 
+					  :url="`/pages/goods/goods?id=${item.id}`"
+					  hover-class="none"
+					>
 						<image class="cover"
-							src="https://yanxuan-item.nosdn.127.net/c07edde1047fa1bd0b795bed136c2bb2.jpg"></image>
+						  :src="item.image"
+						>
+						</image>
 						<view class="meta">
-							<view class="name ellipsis">ins风小碎花泡泡袖衬110-160cm</view>
-							<view class="type">藏青小花， 130</view>
+							<view class="name ellipsis">{{item.name}}</view>
+							<view class="type">{{item.attrsText}}</view>
 							<view class="price">
 								<view class="actual">
 									<text class="symbol">¥</text>
-									<text>99.00</text>
+									<text>{{item.curPrice}}</text>
 								</view>
 							</view>
-							<view class="quantity">x1</view>
+							<view class="quantity">x{{item.quantity}}</view>
 						</view>
 					</navigator>
 					<!-- 待评价状态:展示按钮 -->
@@ -174,15 +197,15 @@
 				<view class="total">
 					<view class="row">
 						<view class="text">商品总价: </view>
-						<view class="symbol">99.00</view>
+						<view class="symbol">{{order.totalMoney}}</view>
 					</view>
 					<view class="row">
 						<view class="text">运费: </view>
-						<view class="symbol">10.00</view>
+						<view class="symbol">{{order.postFee}}</view>
 					</view>
 					<view class="row">
 						<view class="text">应付金额: </view>
-						<view class="symbol primary">109.00</view>
+						<view class="symbol primary">{{order.payMoney}}</view>
 					</view>
 				</view>
 			</view>
@@ -194,7 +217,7 @@
 					<view class="item">
 						订单编号: {{ query.id }} <text class="copy" @tap="onCopy(query.id)">复制</text>
 					</view>
-					<view class="item">下单时间: 2023-04-14 13:14:20</view>
+					<view class="item">下单时间: {{order?.payLatestTime}}</view>
 				</view>
 			</view>
 
